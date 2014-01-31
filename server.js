6d22024becda0266db77337c07bfa8148f456c9f
Yuhang Xu <xuyuhang1005@gmail.com>
2014-01-30 18:10:04 -0800

    Add cross origin.

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ var stops = require('./routes/stops');
 
 var app = express();
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', config.allowedDomains);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
 app.configure(function () {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -16,6 +25,7 @@ app.configure(function () {
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(allowCrossDomain);
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
